18e05eeaa95a781f1fd059c49442ed3a03b05e87
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-08 19:03:03 +0200

    migration created for creating jointure table

--- db/migrate/20200908165030_create_joint_profile_sports.rb
+++ db/migrate/20200908165030_create_joint_profile_sports.rb
@@ -1,6 +1,8 @@
 class CreateJointProfileSports < ActiveRecord::Migration[5.2]
   def change
     create_table :joint_profile_sports do |t|
+      t.references :profile, index: true
+      t.references :sport, index: true
 
       t.timestamps
     end

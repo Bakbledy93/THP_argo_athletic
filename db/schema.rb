52f4c14720c500e77d0fb43c2eb74eefe173165e
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 14:18:19 +0200

    Migration passed for model Level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_120958) do
+ActiveRecord::Schema.define(version: 2020_09_09_121727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2020_09_09_120958) do
   end
 
   create_table "extypes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "levels", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

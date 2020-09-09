348fa4230d35ec8584d16998289853c8ea4c5d04
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 11:48:07 +0200

    Migration created for model TrainingMethodExercise with relationship in models

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,5 @@
 class Exercise < ApplicationRecord
+  has_many :training_method_exercise
+  has_many :training_methods ,through: :training_method_exercise
+
 end
--- app/models/training_method.rb
+++ app/models/training_method.rb
@@ -1,3 +1,6 @@
 class TrainingMethod < ApplicationRecord
   belongs_to :capacity
+
+  has_many :training_method_exercise
+  has_many :exercises ,through: :training_method_exercise
 end

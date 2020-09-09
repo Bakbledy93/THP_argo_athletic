75b1f3dd7f4f597703c262cd22b246f5b374bf11
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 11:32:04 +0200

    Creating migration for model Quality and relationships

--- app/models/capacity.rb
+++ app/models/capacity.rb
@@ -2,4 +2,6 @@ class Capacity < ApplicationRecord
   has_many :roles_muscular_group_capacities
   has_many :sport_roles, through: :roles_muscular_group_capacities
   has_many :muscular_groups, through: :roles_muscular_group_capacities
+
+  belongs_to :quality
 end

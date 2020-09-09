5499423f0e6a7fd2be04ac60afdc5d1028d04cac
samielatri <sami.elatri@yahoo.com>
2020-09-09 17:06:49 +0200

    update chatroomsController with show method

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,8 @@
 class ChatroomsController < ApplicationController
   def show
-
+    # display new message
+    @message = Message.new
+    # display all existing messages
+    @messages = Message.all
   end
 end

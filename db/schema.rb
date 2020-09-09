491ff746ea53df4ff2ae86a13107d497414a04cb
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 15:38:45 +0200

    conflit define in ActiveRecord schema with other branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_09_09_124646) do
-=======
 ActiveRecord::Schema.define(version: 2020_09_09_133116) do
->>>>>>> UserEditingForms
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

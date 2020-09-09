06db7df1af90e2fc65ba8cd4e84aa9ec7bd84a6d
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 14:43:54 +0200

    Migration passed for model Intensity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_122728) do
+ActiveRecord::Schema.define(version: 2020_09_09_124224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2020_09_09_122728) do
   end
 
   create_table "extypes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "intensities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

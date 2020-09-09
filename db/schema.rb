1b35fd0f88741aa2c79bd24b04b6ae36b05dbf6e
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 11:36:03 +0200

    Migration passed for model Quality and relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_213358) do
+ActiveRecord::Schema.define(version: 2020_09_09_093020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2020_09_08_213358) do
     t.integer "height"
     t.integer "weight"
     t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "qualities", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

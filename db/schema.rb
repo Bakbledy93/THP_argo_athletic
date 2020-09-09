a5faf49f73cd165c40f0f06e5c0f911aa8f5e1ac
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 12:15:32 +0200

    Adding quality id to Capacity Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_095410) do
+ActiveRecord::Schema.define(version: 2020_09_09_101322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_09_09_095410) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "quality_id"
+    t.index ["quality_id"], name: "index_capacities_on_quality_id"
   end
 
   create_table "exercises", force: :cascade do |t|
@@ -115,6 +117,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_095410) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "capacities", "qualities"
   add_foreign_key "roles_muscular_group_capacities", "capacities"
   add_foreign_key "roles_muscular_group_capacities", "muscular_groups"
   add_foreign_key "roles_muscular_group_capacities", "sport_roles"

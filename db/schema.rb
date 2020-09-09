1a471a9c714936c4b63189f4a1d34e9d9cef94ac
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 11:43:14 +0200

    Migration passed for model Exercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_093648) do
+ActiveRecord::Schema.define(version: 2020_09_09_094130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "capacities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

34d455bcf4219ea12b58aa9beb92bcc2a77eae5d
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-09 11:57:02 +0200

    Column type deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_094537) do
+ActiveRecord::Schema.define(version: 2020_09_09_095410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_09_09_094537) do
     t.datetime "date_of_birth"
     t.integer "height"
     t.integer "weight"
-    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

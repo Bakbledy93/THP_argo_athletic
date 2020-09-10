639d6dc65b05617aa50d9bf629c5644799012f6c
bakbledy93 <phim.kevin@gmail.com>
2020-09-11 00:19:09 +0200

    import CSV MethodExercise

---
+++ test/controllers/training_method_exercises_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class TrainingMethodExercisesControllerTest < ActionDispatch::IntegrationTest
+  test "should get import" do
+    get training_method_exercises_import_url
+    assert_response :success
+  end
+
+end

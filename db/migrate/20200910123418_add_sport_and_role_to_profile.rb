a067a9fa8b358a5c3ec5a90d03e76ec8e8d2cc16
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-10 14:38:38 +0200

    Migration passed for columns Sport and Sport Role creation in Profile Model

--- db/migrate/20200910123418_add_sport_and_role_to_profile.rb
+++ db/migrate/20200910123418_add_sport_and_role_to_profile.rb
@@ -1,6 +1,6 @@
 class AddSportAndRoleToProfile < ActiveRecord::Migration[5.2]
   def change
-    add_column :profiles, :sport
-    add_column :profiles, :sport_role
+    add_column :profiles, :sport, :string
+    add_column :profiles, :sport_role, :string
   end
 end

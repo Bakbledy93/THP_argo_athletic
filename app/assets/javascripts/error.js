0ab594f326f423d3ceb91ed6d60c98b1e4ce1387
samielatri <sami.elatri@yahoo.com>
2020-09-10 20:07:49 +0200

    reorganized the fade after time script into a function with the advantage to change at ease the fade out time thanks to a variable

--- app/assets/javascripts/error.js
+++ app/assets/javascripts/error.js
@@ -1,7 +1,18 @@
-$(document).ready(function(){
-  setTimeout(function(){
-    $('#notice_wrapper').fadeOut("slow", function() {
-      $(this).remove();
-    })
-  }, 1800) ;
-});
+/* function to fade out the error alert after a determined time */
+function fade_after_time(fade_out_time) {
+
+  $(document).ready(function(){
+    setTimeout(function(){
+      $('#notice_wrapper').fadeOut("slow", function() {
+        $(this).remove();
+      })
+    }, fade_out_time) ;
+  });
+}
+
+/* main */
+
+// variable that defines in seconds the fade out time
+let fade_out_time = 1800 ;
+// call function to fade out the error alter after a determined time
+fade_after_time(fade_out_time) ;

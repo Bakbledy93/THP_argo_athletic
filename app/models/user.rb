fa4dc7bfdeea709f6125155c7f94c43a781554b4
samielatri <sami.elatri@yahoo.com>
2020-09-11 08:47:09 +0200

    added dependent destroy on User s profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :profile
+  has_one :profile, dependent: :destroy
   has_many :messages, dependent: :destroy
 
   has_one_attached :avatar

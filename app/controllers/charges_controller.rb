69693af0154ce254233edabfda4b3c2021a6529f
bakbledy93 <phim.kevin@gmail.com>
2020-09-16 16:42:34 +0200

    subscription feature

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -65,11 +65,6 @@ class ChargesController < ApplicationController
   end
 
   def subscribe
-    if current_user.stripe_id.nil?
-      redirect_to success_path, :flash => {:error => 'Firstly you need to enter your card'}
-      return
-    end
-    #if there is no card
 
     customer = Stripe::Customer.new current_user.stripe_id
     #we define our customer
@@ -81,13 +76,16 @@ class ChargesController < ApplicationController
     #we delete all subscription that the customer has. We do this because we don't want that our customer to have multiple subscriptions
 
     plan_id = params[:plan_id]
+    puts '*'*150
+    puts plan_id
+    puts '*'*150
     subscription = Stripe::Subscription.create({
       customer: customer,
       items: [{plan: plan_id}], })
 #we are creating a new subscription with the plan_id we took from our form
 
     subscription.save
-    redirect_to success_path
+    redirect_to root_path
   end
 
 end

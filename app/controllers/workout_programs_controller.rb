8c4583539dcd554f0e09a679c8167d3e8a3846f3
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-16 15:37:51 +0200

    Updating form for creating workoutPrograms - We can create three differents proframs - bug sometimes two exercises instead of three

--- app/controllers/workout_programs_controller.rb
+++ app/controllers/workout_programs_controller.rb
@@ -12,6 +12,7 @@ class WorkoutProgramsController < ApplicationController
     @workout_program = WorkoutProgram.new
     @user = current_user
     @profile = current_user.profile
+    @profile_id = current_user.profile.id
     # @priority = WorkoutProgram.get_priority
     @level = @user.profile.level
     @sport = @user.profile.sport
@@ -21,61 +22,93 @@ class WorkoutProgramsController < ApplicationController
     @recuperation = Level.where(name: @level).first.recuperations
     @serie = Level.where(name: @level).first.series
 
-    @rolesMGcap = RolesMuscularGroupCapacity.where(sport_role_id: @role_id).where(priority: 1)
+    @rolesMGcap1 = RolesMuscularGroupCapacity.where(sport_role_id: @role_id).where(priority: 1)
+    @cap_id1 = @rolesMGcap1.first.capacity_id
+    @training_methods1 = TrainingMethod.where(capacity_id: @cap_id1)
+    @mg_id1 = @rolesMGcap1.first.muscular_group_id
+    @muscles1 = Muscle.where(muscular_group_id: @mg_id1)
+
     @rolesMGcap2 = RolesMuscularGroupCapacity.where(sport_role_id: @role_id).where(priority: 2)
-    @rolesMGcap3 = RolesMuscularGroupCapacity.where(sport_role_id: @role_id).where(priority: 2)
-    @cap_id = @rolesMGcap.first.capacity_id
-    @training_methods = TrainingMethod.where(capacity_id: @cap_id)
-    @mg_id = @rolesMGcap.first.muscular_group_id
+    @cap_id2 = @rolesMGcap2.first.capacity_id
+    @training_methods2 = TrainingMethod.where(capacity_id: @cap_id2)
+    @mg_id2 = @rolesMGcap2.first.muscular_group_id
+    @muscles2 = Muscle.where(muscular_group_id: @mg_id2)
 
-    @muscles = Muscle.where(muscular_group_id: @mg_id)
+    @rolesMGcap3 = RolesMuscularGroupCapacity.where(sport_role_id: @role_id).where(priority: 3)
+    @cap_id3 = @rolesMGcap3.first.capacity_id
+    @training_method = TrainingMethod.where(capacity_id: @cap_id3)
+    @mg_id3 = @rolesMGcap3.first.muscular_group_id
+    @muscles3 = Muscle.where(muscular_group_id: @mg_id3)
 
-    arr1 = []
-    @training_methods.sample.exercises.uniq.each do |t|
-      arr1 << t.name
+
+    ex_array_A = []
+    @training_methods1.sample.exercises.uniq.each do |t|
+      ex_array_A << t.name
     end
 
-    arr2 = []
-    @muscles.each do |m|
+    ex_array_B = []
+    @muscles1.each do |m|
       m.exercises.each do |x|
-        arr2  << x.name
+        ex_array_B  << x.name
       end 
     end
 
-    @exercises_arr = arr1 & arr2
+    @ex_array1 = ex_array_A & ex_array_B
 
-    @variants_arr = []
-    @exercises_arr.each do |ex|
-      puts "ex : "
-      puts ex
-      puts "var :"
-      @variants_arr << Exercise.where(name: ex).first.variants.uniq.sample.name 
+    @var_array1 = []
+    @ex_array1.each do |ex|
+      @var_array1 << Exercise.where(name: ex).first.variants.uniq.sample.name 
     end
-    ##################################
-    
-    
-    # @profiles_id_arr = WorkoutProgram.array_id_creator(Profile)
-    # @exercises_name_arr = WorkoutProgram.array_name_creator(Exercise)
-    # @training_methods_name_arr = WorkoutProgram.array_name_creator(TrainingMethod)
-    # @levels_name_arr = WorkoutProgram.array_name_creator(Level)
-    
-
-    # @mg = MuscularGroup.find(@mg_id)
-    
-    # @cap = Capacity.find(@cap_id)
-    # @qual = @cap.quality
-
-    ##################################
 
   end
 
   def create
-    @workout_program = WorkoutProgram.create!(workout_program_params)
-    @workout_program = WorkoutProgram.create!(workout_program_params)
-    @workout_program = WorkoutProgram.create!(workout_program_params)
+    @workout_program = WorkoutProgram.new
+    @user = current_user
+    @profile = current_user.profile
+    @profile_id = current_user.profile.id
+    # @priority = WorkoutProgram.get_priority
+    @level = @user.profile.level
+    @sport = @user.profile.sport
+    @role = @user.profile.sport_role
+    @role_id = SportRole.where(name: @role).first.id
+    @repetitions = Level.where(name: @level).first.repetitions
+    @recuperation = Level.where(name: @level).first.recuperations
+    @serie = Level.where(name: @level).first.series
+
+    @rolesMGcap1 = RolesMuscularGroupCapacity.where(sport_role_id: @role_id).where(priority: 1)
+    @cap_id1 = @rolesMGcap1.first.capacity_id
+    @training_methods1 = TrainingMethod.where(capacity_id: @cap_id1)
+    @mg_id1 = @rolesMGcap1.first.muscular_group_id
+    @muscles1 = Muscle.where(muscular_group_id: @mg_id1)
+
+    @rolesMGcap2 = RolesMuscularGroupCapacity.where(sport_role_id: @role_id).where(priority: 2)
+    @cap_id2 = @rolesMGcap2.first.capacity_id
+    @training_methods2 = TrainingMethod.where(capacity_id: @cap_id2)
+    @mg_id2 = @rolesMGcap2.first.muscular_group_id
+    @muscles2 = Muscle.where(muscular_group_id: @mg_id2)
+
+    @rolesMGcap3 = RolesMuscularGroupCapacity.where(sport_role_id: @role_id).where(priority: 2)
+    @cap_id3 = @rolesMGcap3.first.capacity_id
+    @training_methods3 = TrainingMethod.where(capacity_id: @cap_id3)
+    @mg_id3 = @rolesMGcap3.first.muscular_group_id
+    @muscles3 = Muscle.where(muscular_group_id: @mg_id3)
+
+    @ex_array1 = creating_array_exercises(@training_methods1, @muscles1)
+    @ex_array2 = creating_array_exercises(@training_methods2, @muscles2)
+    @ex_array3 = creating_array_exercises(@training_methods3, @muscles3)
+
+    @var_array1 = creating_array_variants(@ex_array1)
+    @var_array2 = creating_array_variants(@ex_array2)
+    @var_array3 = creating_array_variants(@ex_array3)
+
+    create_workout_program
 
     flash[:notice] = "Le programme d'entraînement à été crée"
     redirect_to user_path(current_user.id)
+
+    # @workout_program = WorkoutProgram.create!(workout_program_params)
+
   end
 
   def edit
@@ -96,4 +129,71 @@ class WorkoutProgramsController < ApplicationController
   def current_workout_program
     @workout_program = WorkoutProgram.find(params[:id])
   end
+
+  def create_workout_program
+    @workout_program1 = WorkoutProgram.create!(
+      exercise: @ex_array1, 
+      training_method: @training_methods1.sample.name, 
+      level:  @level, 
+      repetition: @repetitions, 
+      recuperation: @recuperation , 
+      profile_id: @profile_id, 
+      serie: @serie, 
+      variant: @var_array1 
+    )
+    @workout_program2 = WorkoutProgram.create!(
+      exercise: @ex_array2, 
+      training_method: @training_methods2.sample.name, 
+      level:  @level, 
+      repetition: @repetitions, 
+      recuperation: @recuperation , 
+      profile_id: @profile_id, 
+      serie: @serie, 
+      variant: @var_array2 
+    )
+    @workout_program3 = WorkoutProgram.create!(
+      exercise: @ex_array3, 
+      training_method: @training_methods3.sample.name, 
+      level:  @level, 
+      repetition: @repetitions, 
+      recuperation: @recuperation , 
+      profile_id: @profile_id, 
+      serie: @serie, 
+      variant: @var_array3 
+    )
+  end
+
+
+  # def rolesMGcap(role_id, priority)
+  #   RolesMuscularGroupCapacity.where(sport_role_id: role_id).where(priority: priority)
+  # end
+
+
+  def creating_array_exercises(training_method, muscles)
+
+    ex_array_A = []
+    training_method.sample.exercises.uniq.each do |t|
+      ex_array_A << t.name
+    end
+
+    ex_array_B = []
+    muscles.each do |m|
+      m.exercises.each do |x|
+        ex_array_B  << x.name
+      end 
+    end
+
+    ex_array = ex_array_A & ex_array_B
+
+  end 
+
+  def creating_array_variants(ex_array)
+    var_array = []
+    ex_array.each do |ex|
+      var_array << Exercise.where(name: ex).first.variants.uniq.sample.name 
+    end
+    var_array
+  end
+
+  
 end

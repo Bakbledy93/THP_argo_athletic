2ad6c22be1949c5756c0a0dc631d3abc5beb8fb0
samielatri <sami.elatri@yahoo.com>
2020-09-16 11:26:47 +0200

    fancy message added, change to abort

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -8,11 +8,9 @@ module Accessible
   def check_user
     if current_admin
       flash.clear
-      # if you have rails_admin. You can redirect anywhere really
       redirect_to(admins_path) and return
     elsif current_user
       flash.clear
-      # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
       redirect_to (user_path(current_user.id)) and return
     end
   end

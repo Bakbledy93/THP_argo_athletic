3f955876ac19c06a3b7420bad880f4b9284d81ee
Juan Jose Mora <juanmora.dev@gmail.com>
2020-09-17 15:46:29 +0200

    Edit profile Validations 1, not finished

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -29,10 +29,17 @@ class ProfileController < ApplicationController
   end
 
   def update
+    @id = current_user.id
+    @weight = params[:weight]
     @sports = Sport.all
     @profile = Profile.find(params[:id])
-    @profile.update(params.require(:profile).permit(:first_name, :last_name, :weight, :height, :sport, :sport_role, :date_of_birth, :level))
-    flash[:alert] = "Le Profil à été mis à jour"
-    redirect_to user_path(params[:id])
+    if @profile.valid?
+      @profile.update(params.require(:profile).permit(:first_name, :last_name, :weight, :height, :sport, :sport_role, :date_of_birth, :level))
+      flash[:alert] = "Le Profil à été mis à jour"
+      redirect_to user_path(params[:id])
+    else
+      flash[:error] = "S'il vous plait, completez tous les champs du formulaire avec le format correct"
+      redirect_to edit_user_profile_path(id: @profile, user_id: @id)
+    end
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,6 +5,21 @@ class Profile < ApplicationRecord
   has_many :sports, through: :sport_profiles
   has_many :workout_programs
 
+  # validates :email, 
+  #   presence: true,
+  #   uniqueness: true,
+  #   format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+  validates :first_name, 
+    presence: true,
+    length: { minimum: 3 },
+    format: { with: /\A\D+\z/}
+  # validates :first_name, presence: true, format: {}
+  validates :weight, 
+    presence: true,
+    format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
+
   def self.sport_array_creator
     sports = Sport.all
     arr_sport = []

a5fefd7b2d94ee0fe9dd24ecf59f93ae6810bdd2
samielatri <sami.elatri@yahoo.com>
2020-09-19 01:41:41 +0200

    error fix

--- db/migrate/20200915081321_drop_muscle_variants_table.rb
+++
@@ -1,9 +0,0 @@
-class DropMuscleVariantsTable < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :muscle_variants
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end

015131c9269ae7d63b45fa24ee1c2c51bf06e09c
bakbledy93 <phim.kevin@gmail.com>
2020-09-19 13:57:34 +0200

    404 page feature

--- config/routes.rb
+++ config/routes.rb
@@ -115,4 +115,10 @@ Rails.application.routes.draw do
     collection {post :cancel_subscription}
   end
 
+  if Rails.env.production?
+    get '404', :to => 'application#page_not_found'
+    get '422', to: 'application#server_error'
+    get '500', to: 'application#server_error'
+  end
+
 end
